3c7badadfaff006c7ee989311ca6c6f936b85306
rochamauricio <mauricio.mbr@gmail.com>
2021-03-01 22:54:01 -0300

    removendo cc50

--- cc50/arquivos-aulas/week2-0/asciimation/makeit.bat
+++
@@ -1,5 +0,0 @@
-"C:\Program Files\Java\jdk1.5.0_06\bin\javac.exe" -d .\ .\SwPlay.java
-pause
-"C:\Program Files\Java\jdk1.5.0_06\bin\jar.exe" cvf SwPlay.jar .\
-pause
-
--- cc50/treinos/compila.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gcc main.c -o main
-./main
